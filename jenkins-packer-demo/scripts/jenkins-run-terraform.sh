275427b2e63c175546ba0f6fa691a1c9b1546389
rahulgaitonde <gaitonderahul@gmail.com>
2020-03-22 14:05:04 -0400

    Update jenkins-run-terraform.sh

--- jenkins-packer-demo/scripts/jenkins-run-terraform.sh
+++ jenkins-packer-demo/scripts/jenkins-run-terraform.sh
@@ -2,7 +2,8 @@
 set -ex
 AWS_REGION="eu-west-1"
 cd jenkins-packer-demo
-S3_BUCKET="aws s3 ls --region $AWS_REGION | grep terraform-state |tail -n1 |cut -d ' ' -f3"
+#S3_BUCKET="aws s3 ls --region $AWS_REGION | grep terraform-state |tail -n1 |cut -d ' ' -f3"
+S3_BUCKET="`aws s3 ls --region $AWS_REGION | grep terraform-state | tail -n1 | cut -d ' ' -f3`"
 sed -i "'s/terraform-state-rahul/'${S3_BUCKET}'/' backend.tf"
 sed -i 's/#//g' backend.tf
 aws s3 cp '`s3://terraform-state-rahul/amivar.tf amivar.tf --region /$AWS_REGION`'
